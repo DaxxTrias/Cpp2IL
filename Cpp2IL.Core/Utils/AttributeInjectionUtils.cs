221a908c25643605cda16738e9d24e76ede6c7f6
Sean <sean.daxxtrias@gmail.com>
2025-02-17 21:30:55 -0600

    more cleanup. not sure if i didnt break something else but the nullref should be resolved now

--- Cpp2IL.Core/Utils/AttributeInjectionUtils.cs
+++ Cpp2IL.Core/Utils/AttributeInjectionUtils.cs
@@ -92,41 +92,13 @@ public static class AttributeInjectionUtils
 
     private static void ApplyAttributeUsageAttribute(ApplicationAnalysisContext appContext, MultiAssemblyInjectedType multiAssemblyInjectedType, AttributeTargets attributeTargets, bool allowMultiple)
     {
-        System.Diagnostics.Debugger.Launch();
-        var mscorlibAssembly = appContext.GetAssemblyByName("mscorlib");
-        if (mscorlibAssembly == null)
-        {
-            throw new NullReferenceException("mscorlibAssembly is null");
-        }
-
+        var mscorlibAssembly = appContext.GetAssemblyByName("mscorlib") ?? throw new("Could not find mscorlib");
         var targetsEnumType = GetAttributeTargetsType(mscorlibAssembly);
-        if (targetsEnumType == null)
-        {
-            throw new NullReferenceException("targetsEnumType is null");
-        }
-
-        var usageAttribute = mscorlibAssembly.GetTypeByFullName($"System.{nameof(AttributeUsageAttribute)}");
-        if (usageAttribute == null)
-        {
-            throw new NullReferenceException("usageAttribute is null");
-        }
-
-        var usageConstructor = usageAttribute.Methods.FirstOrDefault(m => (m.Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Public && m.Name == ".ctor");
-        if (usageConstructor == null)
-        {
-            throw new NullReferenceException("usageConstructor is null");
-        }
-
-        var allowMultipleProperty = usageAttribute.Properties.FirstOrDefault(p => p.Name == nameof(AttributeUsageAttribute.AllowMultiple));
-        if (allowMultipleProperty == null)
-        {
-            throw new NullReferenceException("allowMultipleProperty is null");
-        }
-
+        var usageAttribute = mscorlibAssembly.GetTypeByFullName($"System.{nameof(AttributeUsageAttribute)}") ?? throw new("Could not find AttributeUsageAttribute");
+        var usageConstructor = usageAttribute.Methods.First(m => (m.Attributes & MethodAttributes.MemberAccessMask) == MethodAttributes.Public && m.Name == ".ctor");
+        var allowMultipleProperty = usageAttribute.Properties.First(p => p.Name == nameof(AttributeUsageAttribute.AllowMultiple));
         foreach (var injectedType in multiAssemblyInjectedType.InjectedTypes)
         {
-            injectedType.InitializeCustomAttributeData();
-
             var newAttribute = new AnalyzedCustomAttribute(usageConstructor);
             var enumParameter = new CustomAttributeEnumParameter(targetsEnumType, appContext, newAttribute, CustomAttributeParameterKind.ConstructorParam, 0);
             enumParameter.UnderlyingPrimitiveParameter.PrimitiveValue = (int)attributeTargets;
